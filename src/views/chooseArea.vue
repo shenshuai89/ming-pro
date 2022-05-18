194decd634c10138e6d6e0a2aa026e21dee9791b
申帅 <shuai.shen@ersoft.cn>
2022-05-18 22:01:45 +0800

    feat:完成数据选择，向父组件发送数据

--- src/views/chooseArea.vue
+++ src/views/chooseArea.vue
@@ -1,6 +1,20 @@
 <template>
-  <m-choose-area></m-choose-area>
+  <m-choose-area @change="showArea"></m-choose-area>
+  <p style="text-align: left">您选择了：{{ selectArea }}</p>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+const selectArea = ref<string>("");
+type areaType = { name: string; code: string };
+interface IAreaData {
+  province: areaType;
+  city: areaType;
+  area: areaType;
+}
+const showArea = (value: IAreaData) => {
+  const { province, city, area } = value;
+  selectArea.value = province.name + " " + city.name + " " + area.name;
+};
+</script>
 <style lang="scss" scoped></style>
