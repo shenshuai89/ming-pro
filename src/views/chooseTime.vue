2636eb12e7a9ad0efa719d00fecc6884cac6b78f
申帅 <shuai.shen@ersoft.cn>
2022-05-20 00:06:27 +0800

    feat: 设置日期选择器choose-date组件

--- src/views/chooseTime.vue
+++ src/views/chooseTime.vue
@@ -5,6 +5,12 @@
       @endChange="handleEnd"
       :startOptions="startOptions"
     ></m-choose-time>
+    <br /><br />
+    <m-choose-date
+      :disableToday="true"
+      @startChange="handleDateStart"
+      @endChange="handleDateEnd"
+    ></m-choose-date>
   </div>
 </template>
 
@@ -18,6 +24,12 @@ const handleStart = (val: string) => {
 };
 const handleEnd = (val: string) => {
   console.log(val, "结束时间");
+};
+const handleDateStart = (val: string) => {
+  console.log(val, "开始日期");
+};
+const handleDateEnd = (val: string) => {
+  console.log(val, "结束日期");
 };
 </script>
 <style lang="scss" scoped></style>
