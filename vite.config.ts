37ca7ec2ae57e296d54175a3094544499964f786
申帅 <shuai.shen@ersoft.cn>
2022-05-19 15:14:44 +0800

    feat: 支持jsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx"; //添加对jsx的支持
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       "@": "/src",
